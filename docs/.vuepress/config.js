d7d8296195f8c90101f2b1c02f8c8fcee9f78b21
Mikael von Ketelhodt <vonketelhodtm@gmail.com>
2019-05-03 18:59:21 +0200

    Initial

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: "Full Stack Day 1",
+  description: "Full Stack Day 1",
+  markdown: {
+    lineNumbers: true
+  }
+};
---
+++ docs/README.md
@@ -0,0 +1,11 @@
+# Full Stack Day 1
+
+``` js{4}
+export default {
+  data () {
+    return {
+      msg: 'Highlighted!'
+    }
+  }
+}
+```
